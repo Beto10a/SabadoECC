00578248b1942f38631b51232b828c6a8dfe9627
ecalvo92 <edcalvo92@hotmail.com>
2024-02-03 10:23:11 -0600

    JWT EN WEB API

--- ProyectoApi_Sabado/ProyectoApi_Sabado/Controllers/UsuarioController.cs
+++ ProyectoApi_Sabado/ProyectoApi_Sabado/Controllers/UsuarioController.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Dapper;
-using System.Data.SqlClient;
+﻿using Microsoft.AspNetCore.Mvc;
+using ProyectoApi_Sabado.Entidades;
+using System.Text;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.VisualBasic;
+using Microsoft.AspNetCore.Authorization;
 
 namespace ProyectoApi_Sabado.Controllers
 {
@@ -10,14 +14,43 @@ namespace ProyectoApi_Sabado.Controllers
     public class UsuarioController : ControllerBase
     {
 
-        [HttpGet]
-        [Route("ConsultarUsuarios")]
-        public IActionResult ConsultarUsuarios(string cedula)
+        [AllowAnonymous]
+        [HttpPost]
+        [Route("IniciarSesion")]
+        public IActionResult ConsultarUsuarios(Usuario entidad)
         {
-            using (var context = new SqlConnection("Server=localhost\\MSSQLSERVER01; Database=BD_SABADO; Trusted_Connection=True"))
-            {
-                return Ok(context.Query<Usuario>("TraerUsuarios", new { cedula }, commandType: System.Data.CommandType.StoredProcedure).FirstOrDefault());
-            }
+            if (entidad.cedula == "304590415" && entidad.contrasenna == "secreta")
+                return Ok(GenerarToken(entidad.cedula));
+
+            return NotFound("Sus credenciales no son correctas");
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("ConsultarDia")]
+        public IActionResult ConsultarDia()
+        {
+            var claims = User.Claims;
+
+            return Ok(DateTime.Now);
+        }
+
+        private string GenerarToken(string cedula)
+        {
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim("username", cedula));
+            claims.Add(new Claim("userrol", "ADMIN"));
+
+            string SecretKey = "erQuPVWaBcnFePyQEGRhDjFCzbtGBLgL";
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(SecretKey));
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(10),
+                signingCredentials: cred);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
     }
